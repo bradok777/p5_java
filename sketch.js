5b7bc2e647034e2ad1853e57c46ddb480fb32ab0
bradok777 <0000111180557xsp@al.educacao.sp.gov.br>
2024-05-24 15:07:23 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 400);
+    background("orange")
+}
+
+function draw() {
+
+  fill("red")
+rect(mouseX,mouseY,100,150,50);//retagungulo(pos x,pos y,tam x,tam y)
+ stroke("blue"); 
+  (mouseIspressed)  background("red")
+}
